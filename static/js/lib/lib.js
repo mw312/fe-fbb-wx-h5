bca0056821ccf933b3814898bc57c294a697af55
zhuyiran <13032126261@163.com>
2017-12-05 17:19:15 +0800

    no message

--- static/js/lib/lib.js
+++ static/js/lib/lib.js
@@ -12,3 +12,6 @@ __inline('avalon/avalon.modern.shim.min.js');
 __inline('shim/es5-shim.min.js');
 
 __inline('echarts.simple.min.js');
+
+__inline('framework/framework7.min.js');
+__inline('framework/my-app.js');
