f4245e09370439aeec2702b906fec7d67bfb9fc0
wei.mo@fangbaba.com <wei.mo@fangbaba.com>
2017-12-11 10:59:41 +0800

    init

--- components/common/js/common.js
+++ components/common/js/common.js
@@ -2,12 +2,10 @@
  * Created by dai on 2016/5/12.
  */
 var date = require('date.js'),
-    constant = require('const.js'),
-    commonUtil = require('common-util.js')
+    CommonUtil = require('common-util.js')
 
 
 module.exports = {
     DATE : date,
-    CONST : constant,
-    COMMONuTIL : commonUtil
+    CommonUtil : CommonUtil
 }
